9b2c237d7d2bc1fe85b6b1c7f6ae233d4b45aaa9
sidharth <sidharthsidhutk@gmail.com>
2024-10-04 11:24:53 +0530

    arrowfunction

--- function1.js
+++ function1.js
@@ -1,5 +1,28 @@
 // arrow function
 
-const sum = (a,b) => a+b
+// const sum = (a,b) => a+b
 
-console.log(sum(2,4));
+// console.log(sum(2,4));
+
+
+
+
+// const arr=[1,2,3,4,5,6]
+
+// function printevan(arr) {
+//     let count=0
+    
+//     for(let i=0;i<=arr.length;i++){
+//         if(arr[i]%2===1){
+//             // console.log(arr[i]);
+//             count++
+
+//         }
+//     }
+//     return count
+    
+// }
+// console.log(printevan(arr));
+
+
+    
